701fd188adaa6a7b64dccadada0a6008c1dcf06b
Ghaithq <gaoia123@gmail.com>
2024-02-05 14:39:38 +0200

    fixed repo

--- repo/Multipliers/BoothMultiplierFullSystem/scripts/setup.tcl
+++
@@ -1,8 +0,0 @@
-set search_path "../ref/models ../ref/icons"
-set link_library  "* saed90nm_typ_ht.db"
-set target_library  "saed90nm_typ_ht.db"
-set symbol_library  "saed90nm.sdb"
-
-alias h history
-alias rc "report_constraint -all_violators"
-
